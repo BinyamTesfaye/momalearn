a672bee333817d30c611dcb484e97fe70ffca6d0
binyam tesfaye <binyamawiw@gmail.com>
2025-08-26 14:40:05 +0300

    h1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -110,13 +110,13 @@ export default function Footer() {
                 <svg className="w-5 h-5 mt-1 mr-3 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                 </svg>
-                <span> +251 91 293 3486</span>
+                <span> +251 900000000</span>
               </li>
               <li className="flex items-start">
                 <svg className="w-5 h-5 mt-1 mr-3 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                 </svg>
-                <span>info@erteban.com</span>
+                <span>info@Moma.com</span>
               </li>
               <li className="flex items-start">
                 <svg className="w-5 h-5 mt-1 mr-3 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -132,7 +132,7 @@ export default function Footer() {
         <div className="border-t border-gray-800 pt-8">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-gray-500 text-center md:text-left mb-4 md:mb-0">
-              © {new Date().getFullYear()} Erteban Charity Organization. All rights reserved.
+              © {new Date().getFullYear()}  Moma Education. All rights reserved.
             </p>
             <div className="flex flex-wrap justify-center gap-4 md:gap-6">
               {['Terms of Service', 'Privacy Policy', 'FAQ', 'Sitemap'].map((item) => (
